1e3e20395e691d6722e1b77744a56fdcce43d634
maira-ag <maira_aguiar@estudante.sesisenai.org.br>
2023-08-18 08:23:01 -0300

    teste

---
+++ logica-javascript/primo.js
@@ -0,0 +1,9 @@
+function primo(num) {
+    for (var i = 2; i < num; i++) {
+      if(num % i === 0) {
+         return false;
+      }
+    }
+    return num !== 1;  
+  }
+  console.log( primo(10) );
