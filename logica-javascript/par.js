fe16f717d7a02d14b7397b9b45fc438f1e870db6
maira-ag <maira_aguiar@estudante.sesisenai.org.br>
2023-08-18 08:19:04 -0300

    teste

---
+++ logica-javascript/par.js
@@ -0,0 +1,15 @@
+function parImpar (numero)
+{
+    if(numero % 2 === 0)
+    {
+        return 'Número Par'
+    }
+    else
+    {
+    return 'Número Ímpar'
+    }
+}
+/*var numero1 = 8;
+var res1 = parImpar(numero1)
+console.log(res1)*/
+console.log(parImpar(5))
